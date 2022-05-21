74f094cbfaa9acc9bb2fd9f3c6ec33303569f5e7
Mona Lisa <denise.demirkol@gmail.com>
2022-05-21 15:31:03 +1000

    Final Version

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,19 +15,15 @@ with open(csvpath) as csvfile:
                     "MinChange":"0",
                     "MaxMonth":"Jan-XX",
                     "MaxChange":"0",
-                    "MeanChange":"0",
-                    "PreviousValue":""}
+                    "PreviousValue":"",
+                    "TotalAmount":"0"}
 
-
-
-   
+    changelist = []
 
     for row in csvreader:
         
-
         if not mydictionary["PreviousValue"]:
-            mydictionary.update({"MinMonth": row[0], "MaxMonth": row[0],"PreviousValue" : row[1] })                        
-
+            mydictionary.update({"MinMonth": row[0], "MaxMonth": row[0],"PreviousValue" : row[1], "TotalAmount" : row[1] })                        
         else:            
 
             if int(row[1]) - int(mydictionary["PreviousValue"]) > int(mydictionary["MaxChange"]):       
@@ -35,18 +31,17 @@ with open(csvpath) as csvfile:
             elif int(row[1]) - int(mydictionary["PreviousValue"])< int(mydictionary["MinChange"]):   
                mydictionary.update({"MinMonth": row[0] , "MinChange":  int(row[1]) - int(mydictionary["PreviousValue"]) })  
 
-            mydictionary.update({"PreviousValue" : row[1] })        
-
-
-
-print(mydictionary)
+            changelist.append (int(row[1]) - int(mydictionary["PreviousValue"]))
+            mydictionary.update({"PreviousValue" : row[1] }) 
+            mydictionary.update({"TotalAmount" : int(mydictionary["TotalAmount"])+int(row[1]) })       
+            
 
 
 
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months: 86")
-print(f"Total: $38382578")
-print(f"Average  Change: $-2315.12")
+print(f"Total Months: {len(changelist)+1}")
+print(f"Total: $"+ str(mydictionary["TotalAmount"]))
+print(f"Average  Change: $" +str(   round(mean(changelist),2) ))
 print(f"Greatest Increase in Profits: " + mydictionary["MaxMonth"] + " ($" +  str(mydictionary["MaxChange"]) + ")")
 print(f"Greatest Decrease in Profits: " + mydictionary["MinMonth"] + " ($"+ str(mydictionary["MinChange"]) +")")
